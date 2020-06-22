4ada7108e44c811bba997d0c80fa8f67cbc7881d
Patryk Stawik <patrykstawik@gmail.com>
2020-06-22 16:26:06 +0200

    alpha version

--- src/pages/MainTab.js
+++ src/pages/MainTab.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useRef} from 'react';
-import { IonContent, IonSlides, IonSlide, IonApp, IonButton, IonIcon, IonImg, IonSelect, IonSelectOption, IonHeader, IonMenu, IonToolbar, IonTitle, IonList, IonItem, IonMenuToggle, IonMenuButton, IonButtons, IonRouterOutlet} from '@ionic/react';
-import {Plugins} from '@capacitor/core';
+import { IonContent, IonSlides, IonSlide, IonApp, IonButton, IonIcon, IonImg, IonSelect, IonSelectOption, IonHeader, IonRippleEffect, IonFab, IonFabButton, IonModal, IonItem, IonInput, IonPopover} from '@ionic/react';
+
+import {optionsOutline, locateOutline} from 'ionicons/icons';
+
+import * as distanceActions from '../data/actions/distanecActions'
 import L from 'leaflet';
 import {connect} from 'react-redux'
 
@@ -13,7 +16,6 @@ import randomLongitude from 'random-longitude'
 
 import 'leaflet/dist/leaflet.css'
 
-import { menuController } from '@ionic/core'
 //const {Geolocation} = Plugins;
 
 import icon from 'leaflet/dist/images/marker-icon.png';
@@ -26,58 +28,72 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
+
+
 const MainTab = (props) => {
   const [latitude, setLatitude] = useState(0)
   const [longitude, setLongitude] = useState(0)
 
-  const [kilometers, setKilometers] = useState(0)
+  const [showPopover, setShowPopover] = useState(false);
+  const kilometersInput = useRef(null)
+
+  const handleClick = ()=>{
+    props.setKilometers(kilometersInput.current.value)
+    setShowPopover(false)
+    
+  }
+
   let geolocation = null;
  
   if (window.navigator && window.navigator.geolocation) {
   geolocation = window.navigator.geolocation;
   }
 
+ 
+
   useEffect(()=>{
     if (geolocation) {
+      
       geolocation.getCurrentPosition(function(position) {
       //console.log(position);
       const crd = position.coords
       setLatitude(crd.latitude)
       setLongitude(crd.longitude)
-      console.log('start: ', latitude, longitude)
-      
+      console.log('moja pozycja: ', latitude, longitude)
+
      })
 }
+
      }, [])
   
+     
+  
+     const position = [latitude, longitude]
+     console.log('position: ', position)
+  
+  
+    
+
  
   
-  
-  
-  
-
-  const position = [latitude, longitude]
-  // console.log(randomLatitude({min: 50, max: 40}));
-  const getKilometers =(e)=>{
-      setKilometers(e.target.value)
-      console.log('km: ', kilometers)}
-  
   const converterDegrees = (e)=>{
-      e.preventDefault()
-     let newLatitude = kilometers * 1/111;
-     let newLongitude = kilometers * 1/(111 * Math.cos(newLatitude));
-     
-     let random_latitude = randomLatitude({min: latitude-newLatitude, max: latitude+newLatitude})
-     let random_longitude = randomLongitude({min: longitude-newLongitude, max: longitude+newLongitude})
-     console.log(random_latitude,random_longitude)
+    //  e.preventDefault()
+    let newLatitude = props.kilometers * 1/111;
+    let newLongitude = props.kilometers * 1/(111 * Math.cos(newLatitude));
+    console.log('kilometry', props.kilometers)
+    let random_latitude = randomLatitude({min: latitude-newLatitude, max: latitude+newLatitude})
 
-     setLatitude(random_latitude);
-     setLongitude(random_longitude);
+    let random_longitude = randomLongitude({min: longitude-newLongitude, max: longitude+newLongitude})
+    console.log('random', random_latitude,random_longitude)
 
+    setLatitude(random_latitude);
+    setLongitude(random_longitude);
+    
+  
      
   }
   const resetFunction=(e)=>{
-      e.preventDefault();
+     // e.preventDefault();
       geolocation.getCurrentPosition(function(position) {
           //console.log(position);
           const crd = position.coords
@@ -88,23 +104,53 @@ const MainTab = (props) => {
   }
   const linkToGoogle = `https://www.google.com/search?q=${latitude},${longitude}`
   
-  console.log(position)
+  
  
   return (
     <IonApp>
-      <IonContent>
-        <IonHeader>Dupa</IonHeader>
-        
-        <Maps center={position} zoom={13} keyboard={0}>
+      <IonContent >
+          <IonHeader className="header">Tu Jesteś</IonHeader>
           
-        <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="made by Patryk Stawik"/>
-        <Marker position={position}>
-    <Popup>Kliknij aby przejść do googla: <br/><a  onClick={()=>window.open(linkToGoogle)}>{`${latitude}, ${longitude}`}</a></Popup>
-    </Marker>
-  </Maps>
-  
+            <IonButton className="ripple-parent" expand="block" color="primary" onClick={converterDegrees}>LOSUJ!
+            <IonRippleEffect type="unbounded"></IonRippleEffect>
+            </IonButton>
+            
+          
+          <Maps center={position} zoom={13} keyboard={0}>
+            
+                  <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="made by Patryk Stawik"/>
+                  <Marker position={position}>
+              <Popup>Kliknij aby przejść do googla: <br/><a  onClick={()=>window.open(linkToGoogle)}>{`${latitude}, ${longitude}`}</a></Popup>
+              </Marker>
+          </Maps>
+          <IonFab vertical="bottom" horizontal="end" slot="fixed">
+            <IonFabButton onClick={()=>setShowPopover(true)}>
+              <IonIcon icon={optionsOutline}/>
+              <IonRippleEffect type="unbounded"></IonRippleEffect>
+            </IonFabButton>
+          </IonFab>
+          <IonFab vertical="bottom" horizontal="start" slot="fixed">
+            <IonFabButton onClick={resetFunction}>
+              <IonIcon icon={locateOutline}/>
+              <IonRippleEffect type="unbounded"></IonRippleEffect>
+            </IonFabButton>
+          </IonFab>
+                <IonPopover
+              isOpen={showPopover}
+              cssClass='my-custom-class'
+              onDidDismiss={e => setShowPopover(false)}
+            >
+              <p>Podaj maksymalną odległość w kilometrach</p>
+                  <IonItem>
+                    <IonInput inputMode="decimal" type="number" 
+                    value={props.kilometers} ref={kilometersInput}/>
+                  </IonItem>
+                  <IonButton expand="block" onClick={handleClick}>Zastosuj
+                  <IonRippleEffect type="unbounded"></IonRippleEffect>
+                  </IonButton>
+            </IonPopover>
         </IonContent>
   </IonApp>
   );
@@ -115,8 +161,14 @@ const MainTab = (props) => {
 const mapStateToProps = (state)=>{
   console.log('mapstate ',state)
   return {
-    kilometers: state.distance.kilometersRadius
+    kilometers: state.distance.kilometersRadius,
+    
   }
 }
 
-export default connect(mapStateToProps)(MainTab);
+const mapDispatchToprops = {
+  setKilometers: distanceActions.setKilometers
+}
+
+
+export default connect(mapStateToProps, mapDispatchToprops)(MainTab);
--- src/pages/Slider.js
+++ src/pages/Slider.js
@@ -12,16 +12,16 @@ import * as distanceActions from '../data/actions/distanecActions'
 const Slider = ({dispatch}) => {
 
   
-  const [randomCord, setRandomCord] = useState(null)
+  
   const [kilometersRadius, setKilometersRadius] = useState(0)
-  const [cityPopulation, setCityPopulation] = useState(0)
+  
   const slidesEl = useRef(document.createElement('ion-slides'))
   const handleSlidesLoad = () => {
     
     slidesEl.current.getActiveIndex().then(res=>{
       // console.log(randomCord)
       // console.log(res)
-      if(res >= 1 && randomCord == null || res>=2 && kilometersRadius == 0 ){
+      if(res >= 1 && kilometersRadius == 0){
         slidesEl.current.lockSwipeToNext(true)
       }else{
         slidesEl.current.lockSwipeToNext(false)
@@ -29,8 +29,7 @@ const Slider = ({dispatch}) => {
      
     })
     dispatch(distanceActions.setKilometers(kilometersRadius))
-    dispatch(distanceActions.setCityPopulation(cityPopulation))
-    dispatch(distanceActions.setRandomCord(randomCord))
+    
   }
   
   const handleKilometers = (e) =>{
@@ -56,34 +55,10 @@ const Slider = ({dispatch}) => {
           </div>
         </IonSlide>
 
-        <IonSlide>
-          
-          <h2>Kilka pytań</h2>
-          <p>Na tym etapie chcemy zadać Ci pytanie czy wolisz odwiedzić <b>totalnie losowe miejsce na mapie</b> (uwaga może to być miejsce trudno dostepne) czy może wolisz udać się do <b>losowego miasta wylosowanego na podstawie Twoich preferencji</b></p>
-          <IonSelect placeholder="Wybierz jedno" 
-          onIonChange={(e)=>setRandomCord(e.detail.value)}>
-            <IonSelectOption value={false}>Losowy Punkt</IonSelectOption>
-            <IonSelectOption value={true}>Losowe Miasto</IonSelectOption>
-          </IonSelect>
-        </IonSlide>
         
         
-        <IonSlide>
-        {randomCord ? 
-        <>
-         <h2>Dopasuj szczegóły swojego miasta</h2>
-         <h5>Podaj maksymalną odległość</h5>
-          <IonItem>
-          <IonInput type="number" 
-            onIonChange={handleKilometers}/>
-          </IonItem>
-          <h5>Podaj minimalną populacje miasta</h5>
-          <IonItem>
-            <IonInput type="number" 
-            onIonChange={e=>setCityPopulation(parseInt(e.detail.value))}/>
-          </IonItem>
-        </>
-          :
+        
+        <IonSlide>      
           <>
           <h2>Jak daleko chcesz jechać ?</h2>
           <p>Podaj maksymalną odległość w kilometrach</p>
@@ -92,7 +67,7 @@ const Slider = ({dispatch}) => {
             onIonChange={handleKilometers}/>
           </IonItem>
           </>
-          }
+          
         </IonSlide>
         
       
